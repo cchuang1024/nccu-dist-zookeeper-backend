8cc70f09e8338829cf9aea45f4f176450d8fe701
ChunChun Huang <cchuang.1024@zoho.com>
2021-06-13 23:50:12 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ZK_URL="127.0.0.1:2181"
-MEDIA_HOST="127.0.0.1"
+MEDIA_HOST=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 
 java -jar media-server.jar --ZK_URL=$ZK_URL --MEDIA_HOST=$MEDIA_HOST
